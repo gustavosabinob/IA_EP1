7b4053ff6de239876c4f50bc5e7d490fd73b6a81
matheus1amorim <40796711+matheus1amorim@users.noreply.github.com>
2021-07-09 17:37:39 -0300

    bla

--- main.py
+++ main.py
@@ -1,41 +1,27 @@
-"""
-    Classe responsavel por por controlar toda a execução do algoritmo Multi-layer Perceptron, tanto para sua fase
-    treinamento, quanto para sua fase de teste
-    Classe que implementa o algoritmo Multi-layer Perceptron (MLP).
-"""
-from pandas import DataFrame
-from sklearn.neural_network import MLPClassifier #implementa o MLP
-from sklearn.model_selection import train_test_split #define os conjuntos de treino e teste
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix #metricas
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
 from data_set import *
-import sys
 import pandas as pd
 import numpy as np
-import matplotlib as plt
 import seaborn as sns
-#from plotnine import *
-#from plotnine.data import mpg
-
+from datetime import datetime
 
 sns.set(style="darkgrid")
-np.set_printoptions(threshold=sys.maxsize)
-#configuar parametros para geração de gráficos
 
 arquivos = Mapper().arquivos
 
-
 for arquivo in arquivos:
 
-    entrada_treino = pd.read_csv('C:/Users/Matheus/Documents/GitHub/IA_EP1/data/' + arquivo['nome_problema'] + '.csv', header=None) #alterar para o caminho da pasta de dados para treino
+    entrada_treino = pd.read_csv('C:/Users/Matheus/Documents/GitHub/IA_EP1/data/' + arquivo['nome_problema'] + '.csv', header=None)
+    ###########alterar para o caminho da pasta de dados para treino##########
     treinador = entrada_treino.drop(labels=63, axis=1)
-    #DÚVIDA
     objetivo = np.squeeze(resultado[arquivo['nome_problema']])
-
 
     treinador_x, treinador_test, objetivo_y, objetivo_test = train_test_split(treinador, objetivo, test_size=7, stratify=objetivo)
 
     mlp = MLPClassifier(hidden_layer_sizes=63,
-                              max_iter=1000,
+                              max_iter=10000,
                               alpha=1e-05,
                               activation='logistic',
                               solver='sgd',
@@ -43,10 +29,8 @@ for arquivo in arquivos:
                               learning_rate_init=0.6,
                               tol=1e-07,
                               verbose=True)
-    #CRIA O MLP
 
-    MLP_fit = mlp.fit(treinador_x, objetivo_y)#altera
-    #ENCAIXA O MODELO DE DADOS x COM O ALVO y
+    MLP_fit = mlp.fit(treinador_x, objetivo_y)
 
     print()
     print('PARAMETROS DE INICIALIZACAO DA REDE \n NUMERO DE NEURONIOS \n')
@@ -64,23 +48,13 @@ for arquivo in arquivos:
     print(f'Penalidade: {mlp.alpha}\n')
 
     print('PARAMETROS FINAIS DA REDE')
-    ###é uma lista de matrizes de peso, em que a matriz de peso no índice i representa os pesos entre a camada i e a camada i + 1.
     print(f'Pesos Camada de Entrada: \n{mlp.coefs_[0]}')
     print(f'Pesos Camada de Saida: \n{mlp.coefs_[1]}')
-    ###é uma lista de vetores de bias, em que o vetor no índice i representa os valores de bias adicionados à camada i + 1.
     print(f'Bias Camada de Entrada: \n{mlp.intercepts_[0]}')
     print(f'Bias Camada de Saida: \n{mlp.intercepts_[1]}\n')
 
-    print('METRICAS')
-    predictions_proba = mlp.predict_proba(treinador_test)
     predictions = mlp.predict(treinador_test)
-    print(f'ACURACIA: {accuracy_score(objetivo_test, predictions)}')
-
-        #curva de erro x iteracao
-    print('--- ERRO X ITERACAO ---\nCurva do erro calculado em funcao da perda x iteracao.\n')
-
-    #import datetime
-    from datetime import datetime
+    print(f'ACURACIA: {accuracy_score(objetivo_test, predictions)}\n')
 
     def generate_loss_graph(mlp, graph_title):
         loss_curve = pd.DataFrame(mlp.loss_curve_)
@@ -89,60 +63,6 @@ for arquivo in arquivos:
         date = datetime.now().strftime("%Y%m%d_%H%M%S")
         graph.savefig(f"results\\graphs\\{graph_title}_{date}.png")
 
-
     generate_loss_graph(mlp, 'curva de erro X iteração')
-    #loss_curve = pd.DataFrame(mlp.loss_curve_)
-    #graph = sns.relplot(ci=None, kind="line", data=loss_curve)
 
-    #graph.savefig(f"results\\graphs\\{graph_title}.png")
-    #plt.pyplot.pie(loss_curve)
-    #sns.lineplot(loss_curve)
-    #graph = sns.FacetGrid(loss_curve)
-    #raph
-    #graph.map_dataframe(sns.scatterplot, x="iterations", y='loss')
-    #sys.stdout.close()
-    #plt.pyplot.plot('iterations', 'loss', data=loss_curve) #+ gg.geom_line()
-
-    ## ESTIMADOR UTILIZADO PARA DEFINIR A MELHOR CONFIGURACAO DA REDE
-    # ###define os parametros que serao combinados pelo GridSearch
-    # parameter_space = {
-    #     'hidden_layer_sizes': [15, 20, 45, 63, 70],
-    #     'activation': ['relu', 'tanh', 'logistic'],
-    #     'solver': ['sgd', 'adam'],
-    #     'alpha': [0.00001, 0.0001, 0.001],
-    #     'learning_rate': ['constant' ,'adaptive'],
-    #     'learning_rate_init': [0.25, 0.5, 0.6, 0.8, 0.7],
-    #     'tol': [0.00001, 0.000001, 0.0001, 0.001, 0.01, 0.0000001]
-    # }
-    #
-    #mlp = MLPClassifier(max_iter = 10000)
-    #
-    # ##chamada que implementa o GridSearch
-    # clf = GridSearchCV(mlp, parameter_space, n_jobs=-1, cv=7)
-    #
-    # ##realiza o fit com para as combinacoes retornadas pelo GridSearch utilizando os parametros pre-definidos
-    # clf.fit(treinador_x, objetivo_y)
-    #
-    # ##log GridSearch
-    # print('Best estimator found:\n', clf.best_estimator_)
-    # print('Best parameters found:\n', clf.best_params_)
-    # print('Best index found:\n', clf.best_index_)
-    # print('Best score index found:\n', clf.best_score_)
-    # print('CV:\n', clf.cv)
-    # print('CV Result:\n', clf.cv_results_)
-    # print('Predict:\n', clf.predict)
-    # print('Error score:\n', clf.error_score)
-    # print('Param grid:\n', clf.param_grid)
-    # print('Multimetric:\n', clf.multimetric_)
-
-    ### METRICAS PARTE3
-    ##matriz de confusao
     print(f'--- MATRIZ DE CONFUSAO ---\n{confusion_matrix(objetivo_test.argmax(axis=1), predictions.argmax(axis=1))}\n')
-
-    ##classificador
-    # print(
-    #     f'--- OUTRAS METRICAS DO CLASSIFICADOR ---\n{classification_report(objetivo_test.argmax(axis=1), predictions.argmax(axis=1))}\n')
-
-##realiza a leitura do csv para pegar os dados para o MLP
-# treinador = pd.read_csv('../inputs/Part-1/caracteres-limpos.csv', header=None)
-# treinador = treinador.drop(labels=63, axis=1)
